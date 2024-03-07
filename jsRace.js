cdea36ad4429442c4bf57291b49992bf40e2a268
username <kindle.maheep@gmail.com>
2024-03-07 08:36:12 +0530

    feat:Made a few changes to animations

--- jsRace.js
+++ jsRace.js
@@ -4,11 +4,12 @@ const cntxt=canv.getContext("2d");
 const cntxt2=canv2.getContext("2d");
 let obstacle1=document.getElementById("obstacle1")
 let car=document.getElementById("car")
-var width=getProperty(canv,"width");
-var length=getProperty(canv,"height");
+//var width=getProperty(canv,"width");
+//var length=getProperty(canv,"height");
 let aVari=0.0;
-
+let gameRunning=true
 //BUILDINGS BELOW
+
 cntxt.fillStyle="rgb(135 206 235)";
 cntxt.fillRect(0,0,canv.width,canv.height/2);
 cntxt.fillStyle="rgb(105 105 105)"
@@ -81,8 +82,8 @@ function getProperty(element,property){
     return parseInt(window.getComputedStyle(element,null).getPropertyValue(property));
     
 }
-console.log(length);
-console.log(width);
+//console.log(length);
+//console.log(width);
 //changed to cntxt2 here so that can use clear rect for animation without affecting the rest of the background
 function makeQuad(x1,y1,x2,y2,w,color){
     cntxt2.fillStyle=color;
@@ -131,15 +132,15 @@ if (arr.length==(the_no-1)*3){
         //console.log("bl")
         //console.log(ogx)
         //console.log("canvas height is",canv.height);
-        console.log("x1 old is ",x1)
-       console.log("y1 old is",y1)
-       console.log("y2 old is",y2)
+        //console.log("x1 old is ",x1)
+       //console.log("y1 old is",y1)
+       //console.log("y2 old is",y2)
         x1=arr[(the_no-1)*3];
         y1=arr[(the_no-1)*3+1];
         y2=arr[(the_no-1)*3+2];
-       console.log("x1 new is ",arr[2])
-       console.log("y1 is",y1)
-       console.log("y2 is",y2)
+       //console.log("x1 new is ",arr[2])
+       //console.log("y1 is",y1)
+       //console.log("y2 is",y2)
     }
     quadWithSlope(m,x1,y1,y2,w,color);
     requestAnimationFrame(()=>animateLanes(m,x1,y1,y2,w,color,speed,the_no));
@@ -172,14 +173,18 @@ function draw(timePassed){
 let x=0;
 let speedOfCar=1;
 
-console.log(car.style.left)
+
 document.addEventListener("keydown",function(e){
-    if (e.key=="ArrowRight"){
+    if (e.key=="ArrowRight"&&car.getBoundingClientRect().left<=canv2.getBoundingClientRect().width){
+        //alert("shoudl work")
+
+
         x+=speedOfCar;
         car.style.left=x+"%"
         
     }
     if (e.key=="ArrowLeft"&&car.style.left>=10+"%"){
+        console.log(car.getBoundingClientRect())
         x-=speedOfCar;
         car.style.left=x+"%"
         
@@ -189,7 +194,9 @@ let timeBeforeNextObstacle=Math.floor(Math.random()*11)
 
 //this will be repeated every 4 seconds and give a time interval b/w 0 to 4 secnds so an obstacle should appear from 4 to 8 seconds apart
 //have to ensure puddle goes away in less than four seconds
+
 let puddleSpeed=2.5
+//let ogObstaclebottom=0
 let slopeOfmotion=1
 let xOfObstacle=0;
 let obstacleOnScreen=false;
@@ -207,9 +214,12 @@ setInterval(function(){
         if(obstacleOnScreen==false){
         let puddleLocation=Math.random()*canv2.width*2/3+10
         obstacle1.style.left=puddleLocation+"px"
+        //ogObstaclebottom=obstacle1.getBoundingClientRect().y;
         timeOfspawn=Date.now()
         obstacle1.style.display="block"
         obstacleOnScreen=true
+        //const ogObstaclebottom=obstacle1.getBoundingClientRect().y
+        
     }
 
             let timer2=setInterval(function(){
@@ -219,7 +229,14 @@ setInterval(function(){
 
             let bottom = obstacle1.getBoundingClientRect().bottom;
             obstacle1.style.top=puddleSpeed*slopeOfmotion*(Date.now()-timeOfspawn)/100+"px";
-            if(parseFloat(obstacle1.style.top.replace("px",""))>=canv2.height/1.6){
+            checkCollision();
+            //2* since should move to roughly teixe its og heigt
+            console.log(obstacle1.getBoundingClientRect())
+            if(obstacle1.getBoundingClientRect().bottom>=1.34*canv2.getBoundingClientRect().height){
+                //parseFloat(obstacle1.style.top.replace("px",""))>=canv2.height/1.6){
+               // consol
+               //alert(ogObstaclebottom)
+               
 
 
                 clearInterval(timer2);
@@ -235,3 +252,11 @@ setInterval(function(){
 
 
 },4000)
+
+function checkCollision(){
+    if (obstacleOnScreen&&(canv2.getBoundingClientRect().height<=obstacle1.getBoundingClientRect().top)&&(car.getBoundingClientRect().x-20<=obstacle1.getBoundingClientRect().x&&obstacle1.getBoundingClientRect().x<=car.getBoundingClientRect().x+20)){
+        alert ("yoo")
+    }
+    
+
+}
